52f9a71d077ef3a143874d402474953636e0c449
Cuong Nguyen <cuongnt53@gmail.com>
2015-12-09 19:44:29 +0700

    ada

---
+++ client/assets/constant.30fa8190.js
@@ -0,0 +1,99 @@
+var App = {
+
+  button: {
+    learnMore : "Learn More",
+    detail : "Detail",
+    search: "Search"
+  },
+
+  homeMenu: {
+    item1 : {name: "Programs", link: "/programs"},
+    item2 : {name: "Partners", link: "/partners"},
+    item3 : {name: "About Us", link: "/about-us"},
+    item4 : {name: "Donate", link: "/donate"}
+  },
+
+  pageMenu: {
+    login: "Log in to your application",
+    item1 : {name: "Program", link: "/program"},
+    item2 : {name: "Apply", link: "/apply"},
+    item3 : {name: "About", link: "/about"},
+    item4 : {name: "Partners", link: "/partners"},
+    item5 : {name: "donate", link: "/donate"}
+  },
+
+  footer: {
+    email: "malnemail@seovn.com",
+    item1 : {name: "Programs.", link: "/programs"},
+    item2 : {name: "Apply.", link: "/apply"},
+    item3 : {name: "Partners.", link: "/partners"},
+    item4 : {name: "About Us.", link: "/about-us"},
+    item5 : {name: "Donate.", link: "/donate"}
+  },
+
+  home : {
+    spotlight: {
+      title: "Make an impact.",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac sodales ipsum. Aenean pellentesque arcu a rhoncus eleifend.",
+      link: "Apply Now"
+    },
+    company : {
+      title: "Company Mission,",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac sodales ipsum. Aenean pellentesque arcu a rhoncus eleifend. Suspendisse at libero tincidunt, consequat lacus a, tempor tellus, consequat lacus a, tempor tellus, tempor tellus.",
+    },
+    "partner" : {
+      title : "Our Partners",
+      description: "Advertise your partners and the industries you offer. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac sodales ipsum. Aenean pellentesque arcu a rhoncus eleifend. Suspendisse at libero tincidunt, consequat lacus a, tempor tellus. Suspendisse at libero tincidunt, consequat lacus a, tempor tellus. . Suspendisse at libero tincidunt, consequat lacus a, tempor tellus."
+    }
+  },
+
+  partners : {
+    title: "Our Partners",
+    sideBarTitle: "View All",
+    sideBar: [
+      {
+        title: "Finance",
+        branches: {
+          golds: [{name: "EY", logo: "EY.png"}, {name: "PwC", logo: "PWC.png"}],
+          silvers: [{name: "Horizon", logo: "Horizon.jpg"}, {name: "VinaCapital", logo: "VinaCapital.jpg"}, {name: "HSC", logo: "HSC.jpg"}],
+          normals:[{name: "VIG", logo: "VIG.png"}, {name: "Indochina Capital", logo: "Indochina_Capital.jpg"}, {name: "ACB", logo: "ACB.png"}, {name: "SSI", logo: "SSI.jpg"}]
+        }
+      }, {
+        title: "Technology",
+        branches: {
+          golds: [],
+          silvers: [],
+          normals:[{name: "M-Service", logo: "M_Service.png"}, {name: "Zalora", logo: "Zalora_Vietnam.jpg"}, {name: "Lazada", logo: "Lazada_VN.png"}, {name: "iVivu", logo: "ivivu.png"}, {name: "Tiki", logo: "Tiki_VN.png"}]
+        }
+      }, {
+        title: "Media",
+        branches: {
+          golds: [{name: "Vietnam Esports", logo: "VN_Esports.png"}],
+          silvers: [],
+          normals:[{name: "VNG", logo: "VNG.jpg"}, {name: "XLE", logo: "XLE_Group.png"}, {name: "YanTV", logo: "YAN.jpg"}]
+        }
+      }, {
+        title: "FMCG",
+        branches: {
+          golds: [{name: "Vinataba-Phillip Morris", logo: "PM-Vinataba.jpg"}],
+          silvers: [{name: "Kinh Do (KDC)", logo: "Kinh_Do.png"}],
+          normals:[]
+        }
+      }, {
+        title: "Ngo",
+        branches: {
+          golds: [],
+          silvers: [],
+          normals:[{name: "San Art", logo: "San_Art.png"}, {name: "LIN", logo: "LIN.png"}]
+        }
+      }, {
+        title: "Education",
+        branches: {
+          golds: [],
+          silvers: [{name: "Everest Education", logo: "E2.gif"}, {name: "Yola", logo: "Yola.jpg"}],
+          normals:[]
+        }
+      }
+    ]
+  }
+};
