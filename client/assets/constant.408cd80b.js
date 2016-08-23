187f4269203e640145a20dd76d0e4561a8977464
seo-vietnam <nhunam89@gmail.com>
2016-08-23 11:48:12 +0700

    Update constant.408cd80b.js

--- client/assets/constant.408cd80b.js
+++ client/assets/constant.408cd80b.js
@@ -90,7 +90,7 @@ var App = {
       rounds: [
         {name: " ", time: "Spring 2017       Summer 2017"},
         {name: "Round 1 (Written Application)", time: "15/08 - 15/09      15/12 - 15/01"},
-        {name: "Round 2 (SEO-V Interview)", time: "10/10 - 23/10     20/02 - 05/03"},
+        {name: "Round 2 (SEO-V Interview)", time: "10/10 - 23/10      20/02 - 05/03"},
         {name: "Round 3 (Employer Interview)", time: "08/11 - 01/12      20/03 - 14/04"},
         {name: "Final Offer", time: "Dec-2016         Apr-2017"}
       ],
